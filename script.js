72bd3b94f2085737bc720c5b0c149520f21b928d
Nicholas <nachos.underwood@gmail.com>
2020-09-20 15:27:09 -0700

    done

---
+++ script.js
@@ -0,0 +1,15 @@
+let textContent = $('#text-content');
+let textHeader = $('#text-header h1');
+let textDate = $('#text-header span');
+
+$('.card').hover(function() {
+    let card = $(this)
+    textContent.html(card.find(".card-body").html());
+    textHeader.text(card.find(".card-header h2").text());
+    textDate.text(card.find(".card-header p").text())
+});
+
+$(document).ready(() => {
+    $('.card:last-of-type').trigger("mouseover");
+    // console.log($('.card:last-of-type'))
+})
